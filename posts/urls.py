3bdc8fdb27cc05c5320a63e6618a9d5059120759
James Davis <james.davisphd@gmail.com>
2020-04-27 21:08:03 -0500

    adding a custom form

--- posts/urls.py
+++ posts/urls.py
@@ -6,8 +6,8 @@ from .views import PostListView, PostCreateView, TextListView, TextCreateView
 urlpatterns = [
     path('posts/new/', PostCreateView.as_view(), name='post_new'),
     path('', PostListView.as_view(), name='home'),
-    path('posts/new/', TextCreateView.as_view(), name='text_new'),
-    path('', TextListView.as_view(), name='home'),
+    # path('posts/new/', TextCreateView.as_view(), name='text_new'),
+    # path('', TextListView.as_view(), name='home'),
 ]
 # urlpatterns = [
 #     path('posts/new/', TextCreateView.as_view(), name='post_new'),
